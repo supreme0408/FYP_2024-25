287e0c20b43632d5c0a57e41dc14e027b116559e
Ramanuj <osupreme0408@gmail.com>
2025-04-23 19:27:32 +0530

    Update

--- finrobot/data_source/__init__.py
+++ finrobot/data_source/__init__.py
@@ -6,9 +6,11 @@ from .fmp_utils import FMPUtils
 from .sec_utils import SECUtils
 from .reddit_utils import RedditUtils
 from .weatherapi_utils import WeatherAPIUtils
+from .soil_data_util import AgriInfoService
 
 
-__all__ = ["FinnHubUtils", "YFinanceUtils", "FMPUtils", "SECUtils","WeatherAPIUtils"]
+
+__all__ = ["FinnHubUtils", "YFinanceUtils", "FMPUtils", "SECUtils","WeatherAPIUtils","WeatherAnalysisInsights","AgriInfoService"]
 
 if importlib.util.find_spec("finnlp") is not None:
     from .finnlp_utils import FinNLPUtils
